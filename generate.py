11a5469276e04f4f7a9c0fb4b34e2fb8a5284d36
elgiano <elgiano@gmail.com>
2023-08-11 19:33:28 +0200

    rave-conditioning: first draft

--- generate.py
+++ generate.py
@@ -4,14 +4,15 @@
 #### Author: Moises Horta Valenzuela / @hexorcismos
 #### Year: 2023
 
+import librosa
 import argparse
 import os
 import torch
 import numpy as np
 import random
 import soundfile as sf
-from tqdm import tqdm
 from audio_diffusion_pytorch import DiffusionModel, UNetV0, VDiffusion, VSampler
+from rave_conditioning import RaveConditioningModel
 
 torch._C._jit_set_profiling_mode(False)
 torch._C._jit_set_profiling_executor(False)
@@ -45,13 +46,17 @@ def parse_args():
     parser.add_argument("--seed_a", type=int, default=random.randint(0,2**31-1), help="Starting seed for interpolation.")
     parser.add_argument("--seed_b", type=int, default=random.randint(0,2**31-1), help="Ending seed for interpolation.")
     parser.add_argument("--temperature", type=float, default=1.0, help="Temperature of the random noise before diffusion.")
+    parser.add_argument("--conditioning", type=str, help="Audio or .pt latents file for conditioning")
+    parser.add_argument("--rave_conditioning", type=str, help="RAVE model to encode the conditioning file. Defaults to --rave_model")
     return parser.parse_args()
+
 
 def slerp(val, low, high):
     omega = torch.acos((low/torch.norm(low, dim=2, keepdim=True) * high/torch.norm(high, dim=2, keepdim=True)).sum(dim=2, keepdim=True).clamp(-1, 1))
     so = torch.sin(omega)
     res = (torch.sin((1.0-val)*omega)/so) * low + (torch.sin(val*omega)/so) * high
     return res
+
 
 # Generate the audio using the provided models and settings.
 def generate_audio(model, rave, args, seed):
@@ -80,21 +85,12 @@ def generate_audio(model, rave, args, seed):
         # diff = model(noise)
         # noise = diff
 
-        diff_mean = diff.mean()
-        diff_std = diff.std()
-        diff = (diff - diff_mean) / diff_std
+        diff = (diff - diff.mean()) / diff.std()
 
         rave = rave.cpu()
         diff = diff.cpu()
         print("Decoding using RAVE Model...")
-        y = rave.decode(diff)
-        y = y.reshape(-1).detach().numpy()
-
-        if rave.stereo:
-            y_l = y[:len(y)//2]
-            y_r = y[len(y)//2:]
-
-            y = np.stack((y_l, y_r), axis=-1)
+        y = decode_latents(diff, rave)
 
         path = f'{args.output_path}/rave-latent_diffusion_seed{seed}_{args.name}_{rave_model_name}.wav'
         print(f"Writing {path}")
@@ -129,24 +125,76 @@ def interpolate_seeds(model, rave, args, seed):
         diff2 = model.sample(noise2, num_steps=args.diffusion_steps, show_progress=True)
         diff = slerp(torch.linspace(0., args.lerp_factor, z_length).to(device), diff1, diff2)
 
-        diff_mean = diff.mean()
-        diff_std = diff.std()
-        diff = (diff - diff_mean) / diff_std
+        diff = (diff - diff.mean()) / diff.std()
 
         rave = rave.cpu()
         diff = diff.cpu()
         print("Decoding using RAVE Model...")
-        y = rave.decode(diff)
-        y = y.reshape(-1).detach().numpy()
-
-        if rave.stereo:
-            y_l = y[:len(y)//2]
-            y_r = y[len(y)//2:]
-            y = np.stack((y_l, y_r), axis=-1)
-
+        y = decode_latents(diff, rave)
         path = f'{args.output_path}/rave-latent_diffusion_seed{seed}_{args.name}_{rave_model_name}_slerp.wav'
         print(f"Writing {path}")
         sf.write(path, y, args.sample_rate)
+
+
+def encode_audiofile(encoder, audiofile_path):
+    with torch.no_grad():
+        cond_y, _ = librosa.load(audiofile_path, sr=encoder.sr, mono=True)
+        cond_y = torch.tensor(cond_y.reshape((1, 1, -1))).to(device)
+        cond_latents = encoder.encode(cond_y)
+
+    return cond_latents
+
+
+def generate_audio_conditioning(model, rave, cond_latents, args, seed):
+    with torch.no_grad():
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
+
+        # rave_dims = get_latent_dim(rave)
+        noise = torch.randn_like(cond_latents)
+        noise = noise * args.temperature
+        z_length = noise.size(-1)
+
+        rave_model_name = os.path.basename(args.rave_model).split(".")[0]
+        diffusion_model_name = os.path.basename(args.model_path)
+
+        print(f"Generating {z_length} latent codes with Diffusion model:", diffusion_model_name)
+        print("Decoding using RAVE Model:", rave_model_name)
+        print("Seed:", seed)
+
+        model.eval()
+
+        ### GENERATING WITH .PT FILE
+        diff = model.sample(noise, conditioning=cond_latents,
+                            num_steps=args.diffusion_steps, show_progress=True)
+        # diff = model(noise)
+        # noise = diff
+
+        diff = (diff - diff.mean()) / diff.std()
+
+        rave = rave.cpu()
+        diff = diff.cpu()
+        print("Decoding using RAVE Model...")
+        y = decode_latents(diff, rave)
+
+        path = f'{args.output_path}/rave-latent_diffusion_seed{seed}_{args.name}_{rave_model_name}.wav'
+        print(f"Writing {path}")
+        sf.write(path, y, args.sample_rate)
+
+
+def decode_latents(latents, rave):
+    y = rave.decode(latents)
+    y = y.reshape(-1).detach().numpy()
+
+    if rave.stereo:
+        y_l = y[:len(y)//2]
+        y_r = y[len(y)//2:]
+
+        y = np.stack((y_l, y_r), axis=-1)
+
+    return y
+
 
 # Main function sets up the models and generates the audio.
 def main():
@@ -160,24 +208,50 @@ def main():
         assert hasattr(rave, "sr"), msg
         args.sample_rate = rave.sr
 
-    ### GENERATING WITH .PT FILE DIFFUSION
-    model = DiffusionModel(
-        net_t=UNetV0,
-        in_channels=rave_dims,
-        channels=[256, 256, 256, 256, 512, 512, 512, 768, 768],
-        factors=[1, 4, 4, 4, 2, 2, 2, 2, 2],
-        items=[1, 2, 2, 2, 2, 2, 2, 4, 4],
-        attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1],
-        attention_heads=12,
-        attention_features=64,
-        diffusion_t=VDiffusion,
-        sampler_t=VSampler,
-    ).to(device)
+    if args.conditioning is None:
+        ### GENERATING WITH .PT FILE DIFFUSION
+        model = DiffusionModel(
+            net_t=UNetV0,
+            in_channels=rave_dims,
+            channels=[256, 256, 256, 256, 512, 512, 512, 768, 768],
+            factors=[1, 4, 4, 4, 2, 2, 2, 2, 2],
+            items=[1, 2, 2, 2, 2, 2, 2, 4, 4],
+            attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1],
+            attention_heads=12,
+            attention_features=64,
+            diffusion_t=VDiffusion,
+            sampler_t=VSampler,
+        )
+    else:
+        # todo: read embedding_features
+        model = RaveConditioningModel(in_channels=rave_dims,
+                                      embedding_features=args.latent_length)
+
+    model = model.to(device)
 
     checkpoint = torch.load(args.model_path, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
 
-    if not args.lerp:
+    if args.conditioning is not None:
+        if os.path.splitext(args.conditioning)[1] == ".pt":
+            print("Loading conditioning latents...")
+            cond_latents = torch.load(args.conditioning)
+        else:
+            if args.rave_conditioning is None:
+                cond_encoder = rave
+            else:
+                cond_encoder = torch.jit.load(args.rave_conditioning).to(device)
+            print("Encoding conditioning audiofile...")
+            cond_latents = encode_audiofile(cond_encoder, args.conditioning)
+
+        cond_latents = torch.nn.functional.pad(
+            cond_latents, (0, -cond_latents.size(-1) % args.latent_length)
+        ).reshape((-1, 16, args.latent_length))
+
+        for i in range(args.num):
+            seed = args.seed + i
+            generate_audio_conditioning(model, rave, cond_latents, args, seed)
+    elif not args.lerp:
         for i in range(args.num):
             seed = args.seed + i
             generate_audio(model, rave, args, seed)
--- train.py
+++ train.py
@@ -8,13 +8,14 @@ import argparse
 import torch.multiprocessing as mp
 import torch
 import os
-import time
 import datetime
 import numpy as np
 import random
 from torch.utils.data import Dataset, DataLoader
 
 from audio_diffusion_pytorch import DiffusionModel, UNetV0, VDiffusion, VSampler
+
+from rave_conditioning import RAVEConditioningModel, RaveConditioningDataset, load_cond_datasets
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -24,19 +25,18 @@ else:
     device = torch.device("cpu")
 current_date = datetime.date.today()
 
+
 class RaveDataset(Dataset):
-    def __init__(self, latent_folder, latent_files):
-        self.latent_folder = latent_folder
-        self.latent_files = latent_files
+    def __init__(self, latent_files):
         self.latent_data = []
 
-        for latent_file in self.latent_files:
-            latent_path = os.path.join(self.latent_folder, latent_file)
+        for latent_path in latent_files:
             z = np.load(latent_path)
             z = torch.from_numpy(z).float().squeeze()
             self.latent_data.append(z)
 
         self.latent_size = self.latent_data[0].shape[0]
+        self.num_latents = self.latent_data[0].shape[-1]
 
     def __len__(self):
         return len(self.latent_data)
@@ -44,20 +44,52 @@ class RaveDataset(Dataset):
     def __getitem__(self, index):
         return self.latent_data[index]
 
+
+def load_dataset(latent_folder, split_ratio):
+    assert os.path.isdir(latent_folder), f"latent folder '{latent_folder}' not found"
+
+    latent_files = [os.path.join(latent_folder, f)
+                    for f in os.listdir(latent_folder) if f.endswith(".npy")]
+
+    assert len(latent_files) > 0, f"no latent files found in '{latent_folder}'"
+
+    random.shuffle(latent_files)
+
+    num_train = int(len(latent_files) * split_ratio)
+    train_dataset = RaveDataset(latent_files[:num_train])
+    val_dataset = RaveDataset(latent_files[num_train:])
+
+    return train_dataset, val_dataset, num_train, len(latent_files) - num_train
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a model with a new dataset.")
-    parser.add_argument("--name", type=str, default=f"run_{current_date}", help="Name of your training run.")
-    parser.add_argument("--latent_folder", type=str, default="./latents/", help="Path to the directory containing the latent files.")
-    parser.add_argument("--checkpoint_path", type=str, default=None, help="Resume training from checkpoint.")
-    parser.add_argument("--save_out_path", type=str, default="./runs/", help="Path to the directory where the model checkpoints will be saved.")
-    parser.add_argument("--split_ratio", type=float, default=0.8, help="Ratio for splitting the dataset into training and validation sets.")
-    parser.add_argument("--max_epochs", type=int, default=25000, help="Maximum epochs to train model.")
-    parser.add_argument("--scheduler_steps", type=int, default=100, help="Diffusion steps for scheduler.")
-    parser.add_argument("--batch_size", type=int, default=8, help="Batch size for training.")
-    parser.add_argument("--accumulation_steps", type=int, default=2, help="Number of gradient accumulation steps.")
-    parser.add_argument("--save_interval", type=int, default=50, help="Interval (number of epochs) at which to save the model.")
-    parser.add_argument("--finetune", type=bool, default=False, help="Finetune model.")
+    parser.add_argument("--name", type=str, default=f"run_{current_date}",
+                        help="Name of your training run.")
+    parser.add_argument("--latent_folder", type=str, default="./latents/",
+                        help="Path to the directory containing the latent files.")
+    parser.add_argument("--checkpoint_path", type=str, default=None,
+                        help="Resume training from checkpoint.")
+    parser.add_argument("--save_out_path", type=str, default="./runs/",
+                        help="Path to the directory where the model checkpoints will be saved.")
+    parser.add_argument("--split_ratio", type=float, default=0.8,
+                        help="Ratio for splitting the dataset into training and validation sets.")
+    parser.add_argument("--max_epochs", type=int, default=25000,
+                        help="Maximum epochs to train model.")
+    parser.add_argument("--scheduler_steps", type=int, default=100,
+                        help="Diffusion steps for scheduler.")
+    parser.add_argument("--batch_size", type=int, default=8,
+                        help="Batch size for training.")
+    parser.add_argument("--accumulation_steps", type=int, default=2,
+                        help="Number of gradient accumulation steps.")
+    parser.add_argument("--save_interval", type=int, default=50,
+                        help="Interval (number of epochs) at which to save the model.")
+    parser.add_argument("--finetune", type=bool, default=False,
+                        help="Finetune model.")
+    parser.add_argument("--cond_latent_folder", type=str, default="./latents/",
+                        help="Path to the directory containing the latent files for conditioning.")
     return parser.parse_args()
+
 
 def set_seed(seed=664):
     torch.manual_seed(seed)
@@ -91,6 +123,7 @@ def main():
     split_ratio = args.split_ratio
     batch_size = args.batch_size
     save_interval = args.save_interval
+    batch_size = args.batch_size
 
     global best_loss
     global best_epoch
@@ -103,33 +136,30 @@ def main():
 
     set_seed(664)
 
-    random.shuffle(latent_files)
-    split_index = int(len(latent_files) * split_ratio)
-    train_latent_files = latent_files[:split_index]
-    val_latent_files = latent_files[split_index:]
+    if not args.cond_latent_folder:
+        train_dataset, val_dataset, num_train_files, num_val_files = load_dataset(latent_folder, split_ratio)
 
-    train_dataset = RaveDataset(latent_folder, train_latent_files)
-    val_dataset = RaveDataset(latent_folder, val_latent_files)
+        model = DiffusionModel(
+            net_t=UNetV0,
+            in_channels=train_dataset.rave_dims,
+            channels=[256, 256, 256, 256, 512, 512, 512, 768, 768],
+            factors=[1, 4, 4, 4, 2, 2, 2, 2, 2],
+            items=[1, 2, 2, 2, 2, 2, 2, 4, 4],
+            attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1],
+            attention_heads=12,
+            attention_features=64,
+            diffusion_t=VDiffusion,
+            sampler_t=VSampler,
+        )
+    else:
+        train_dataset, val_dataset, num_train_files, num_val_files = load_cond_dataset(latent_folder, args.cond_latent_folder, split_ratio)
+        model = RaveConditioningModel(in_channels=train_dataset.rave_dims,
+                                      embedding_features=train_dataset.num_latents)
 
-    rave_dims = train_dataset.latent_size
-
-    batch_size = args.batch_size
+    model = model.to(device)
 
     train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
     val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
-
-    model = DiffusionModel(
-        net_t=UNetV0,
-        in_channels=rave_dims,
-        channels=[256, 256, 256, 256, 512, 512, 512, 768, 768],
-        factors=[1, 4, 4, 4, 2, 2, 2, 2, 2],
-        items=[1, 2, 2, 2, 2, 2, 2, 4, 4],
-        attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1],
-        attention_heads=12,
-        attention_features=64,
-        diffusion_t=VDiffusion,
-        sampler_t=VSampler,
-    ).to(device)
 
     print("Model Architecture:")
     print(model)
@@ -139,8 +169,8 @@ def main():
     print(f"Total number of parameters: {total_params}")
     print(f"Number of trainable parameters: {trainable_params}\n")
 
-    print("Training:", len(train_latent_files))
-    print("Validation:", len(val_latent_files))
+    print("Training:", num_train_files, "files")
+    print("Validation:", num_val_files, "files")
 
     if checkpoint_path != None:
         print(f"Resuming training from: {checkpoint_path}\n")
@@ -164,9 +194,14 @@ def main():
         optimizer.zero_grad()
 
         for step, batch in enumerate(train_data_loader):
-            batch_rave_tensor = batch.to(device)
-
-            loss = model(batch_rave_tensor)
+            if args.conditioning is None:
+                batch_rave_tensor = batch.to(device)
+                loss = model(batch_rave_tensor)
+            else:
+                x, c = batch
+                x_rave_tensor = x.to(device)
+                cond_rave_tensor = c.to(device)
+                loss = model(x_rave_tensor, conditioning=cond_rave_tensor)
 
             train_loss += loss.item()
 
@@ -186,9 +221,14 @@ def main():
 
             val_loss = 0
             for batch in val_data_loader:
-                batch_rave_tensor = batch.to(device)
-
-                loss = model(batch_rave_tensor)
+                if args.conditioning is None:
+                    batch_rave_tensor = batch.to(device)
+                    loss = model(batch_rave_tensor)
+                else:
+                    x, c = batch
+                    x_rave_tensor = x.to(device)
+                    cond_rave_tensor = c.to(device)
+                    loss = model(x_rave_tensor, conditioning=cond_rave_tensor)
 
                 val_loss += loss.item()
 
